4fbfe7c3d06b6dad5610640ebff62fcb7201e21d
hia89g7ufpdew <155465449+hia89g7ufpdew@users.noreply.github.com>
2024-07-17 19:55:55 +0200

    BaseTest
    various 3D stuff

---
+++ Player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+
+@export var player_speed : int = 400
+
+
+func _physics_process(delta):
+	var velocity = Vector3.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
